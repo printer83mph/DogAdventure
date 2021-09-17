17d200ab65120f8f9cdb9f7b8e61716b76f5edfe
printer83mph <printer.83mph@gmail.com>
2021-09-16 22:08:00 -0400

    Work on enemy AI rewrite

--- Assets/Scripts/Enemies/HumanStateMachineBehaviours/HumanIdleBehaviour.cs
+++ Assets/Scripts/Enemies/HumanStateMachineBehaviours/HumanIdleBehaviour.cs
@@ -1,32 +1,51 @@
-﻿using UnityEngine;
+﻿using ScriptableObjects.Audio;
+using UnityEngine;
 
 namespace Enemies.HumanStateMachineBehaviours
 {
     public class HumanIdleBehaviour : StateMachineBehaviour
     {
 
+        // animator static reference stuff
+        private static readonly int Suspicion = Animator.StringToHash("suspicion");
+
         // enemy reference
         private HumanEnemy _enemy;
-
-        // logic
         private float _suspicion;
-
+        
+        // private state things
+        private bool _checkingOutSound;
+        
         public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
             _enemy = animator.GetComponent<HumanEnemy>();
+            // _suspicion = animator.GetFloat(Suspicion);
 
-            _suspicion = 0;
+            _enemy.AudioChannel.playAudio += OnAudio;
+            Debug.Log("you are been trolled");
+        }
 
-            // initialize hooks here
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            _enemy.AudioChannel.playAudio -= OnAudio;
+        }
+
+        private void OnAudio(Vector3 pos, SoundType type, float radius)
+        {
+            Debug.Log("Heard audio bro!");
         }
 
         public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
+            // figure out vision weight addition
             float visionWeight = _enemy.Vision.PlayerVisionWeight;
-            _suspicion += (visionWeight > 0
+            float toAdd = (visionWeight > 0
                 ? visionWeight * _enemy.SuspicionGainSpeed * Time.deltaTime
                 : -_enemy.SuspicionDispelSpeed * Time.deltaTime);
-            _suspicion = Mathf.Clamp(_suspicion, 0, 1);
+            
+            // add to private suspicion value and update state machine's suspicion
+            _suspicion = Mathf.Clamp(_suspicion + toAdd, 0, 1);
+            // animator.SetFloat(Suspicion, _suspicion);
         }
     }
 }
